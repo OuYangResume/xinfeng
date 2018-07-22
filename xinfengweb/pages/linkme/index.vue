8c827ee14373ebae719fd8c133d78a36814019a5
OuYangResume <15180191339@139.com>
2018-07-22 12:20:32 +0800

    添加高德地图

--- xinfengweb/pages/linkme/index.vue
+++ xinfengweb/pages/linkme/index.vue
@@ -16,29 +16,62 @@
                 <div class="grid-content bg-purple">3</div>
             </el-col>
         </el-row>
-        <el-row>
+        <!-- <el-row>
             <el-col>
                 <div id="map">
                 </div>
             </el-col>
-        </el-row>
+        </el-row> -->
+        <div id="map"></div>
     </div>
 
 </template>
 
 <script>
-import L from "leaflet/dist/leaflet.js";
-import "leaflet/dist/leaflet.css";
+import Leaflet from "leaflet";
+import("leaflet/dist/leaflet.css");
+import mapProvider from "~/assets/js/leaflet.MapProviders.js";
 export default {
   data() {
-    return {};
+    return {
+      map: null,
+      map_config: {
+        zoom: 10,
+        center: [37.5, 106],
+        minZoom: 3,
+        maxZoom: 18
+      }
+    };
+  },
+  methods: {
+    initMap: function() {
+      this.map = L.map("map", {
+        center: this.map_config.center,
+        zoom: this.map_config.zoom,
+        minZoom: this.map_config.minZoom,
+        maxZoom: this.map_config.maxZoom,
+        attribution: "&copy; 高德地图",
+        // scrollWheelZoom: false,
+        logo: false
+      });
+      L.tileLayer
+        .mapProvider("GaoDe.Normal.Map", {
+          attribution: this.map_config.attribution
+        })
+        .addTo(this.map);
+    },
+    addMapLayers() {}
+  },
+  mounted() {
+    this.initMap();
   }
 };
 </script>
 
 <style scoped>
 #map {
-  background: burlywood;
   height: 50vh;
+  margin-left: -20px;
+  margin-right: -20px;
 }
 </style>
